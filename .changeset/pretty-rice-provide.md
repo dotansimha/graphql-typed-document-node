2618ddf1ff9c0bea145a637a0a9106ebe3169e78
Dotan Simha <dotansimha@gmail.com>
2023-02-05 11:28:51 +0900

    cleanup, use pnpm and added changesets (#149)

---
+++ .changeset/pretty-rice-provide.md
@@ -0,0 +1,7 @@
+---
+"@graphql-typed-document-node/core": patch
+---
+
+- Cleanup repo structure, ship lightweight type-only package.
+- Use `import type` instead of `import` for `DocumentNode`.
+- Compile and test with latest TS and latest NodeJS LTS.
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,9 @@
-import { DocumentNode } from 'graphql';
+import type { DocumentNode } from "graphql";
 
-export interface TypedDocumentNode<Result = { [key: string]: any }, Variables = { [key: string]: any }> extends DocumentNode {
+export interface TypedDocumentNode<
+  Result = { [key: string]: any },
+  Variables = { [key: string]: any }
+> extends DocumentNode {
   /**
    * This type is used to ensure that the variables you pass in to the query are assignable to Variables
    * and that the Result is assignable to whatever you pass your result to. The method is never actually
@@ -12,15 +15,25 @@ export interface TypedDocumentNode<Result = { [key: string]: any }, Variables = 
 /**
  * Helper for extracting a TypeScript type for operation result from a TypedDocumentNode.
  * @example
- * const myQuery = { ... }; // TypedDocumentNode<R, V> 
+ * const myQuery = { ... }; // TypedDocumentNode<R, V>
  * type ResultType = ResultOf<typeof myQuery>; // Now it's R
  */
-export type ResultOf<T> = T extends TypedDocumentNode<infer ResultType, infer VariablesType> ? ResultType : never;
+export type ResultOf<T> = T extends TypedDocumentNode<
+  infer ResultType,
+  infer VariablesType
+>
+  ? ResultType
+  : never;
 
 /**
  * Helper for extracting a TypeScript type for operation variables from a TypedDocumentNode.
  * @example
- * const myQuery = { ... }; // TypedDocumentNode<R, V> 
+ * const myQuery = { ... }; // TypedDocumentNode<R, V>
  * type VariablesType = VariablesOf<typeof myQuery>; // Now it's V
  */
-export type VariablesOf<T> = T extends TypedDocumentNode<infer ResultType, infer VariablesType> ? VariablesType : never;
+export type VariablesOf<T> = T extends TypedDocumentNode<
+  infer ResultType,
+  infer VariablesType
+>
+  ? VariablesType
+  : never;
