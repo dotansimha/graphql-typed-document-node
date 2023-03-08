e896a0fd70aa7cd36be128b4aeccde78ffd98d85
beerose <alexsandra.sikora@gmail.com>
2023-03-08 16:37:47 +0100

    Add TypedDocumentString and DocumentTypeDecoration helper

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,16 +1,22 @@
 import type { DocumentNode } from "graphql";
 
-export interface TypedDocumentNode<
-  Result = { [key: string]: any },
-  Variables = { [key: string]: any }
-> extends DocumentNode {
+export interface DocumentTypeDecoration<TResult, TVariables> {
   /**
    * This type is used to ensure that the variables you pass in to the query are assignable to Variables
    * and that the Result is assignable to whatever you pass your result to. The method is never actually
    * implemented, but the type is valid because we list it as optional
    */
-  __apiType?: (variables: Variables) => Result;
+  __apiType?: (variables: TVariables) => TResult;
 }
+
+export interface TypedDocumentNode<
+  TResult = { [key: string]: any },
+  TVariables = { [key: string]: any }
+> extends DocumentNode,
+    DocumentTypeDecoration<TResult, TVariables> {}
+
+export type TypedDocumentString<TResult, TVariables> = string &
+  DocumentTypeDecoration<TResult, TVariables>;
 
 /**
  * Helper for extracting a TypeScript type for operation result from a TypedDocumentNode.
